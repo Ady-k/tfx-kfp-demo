7d9131912a5a104c7def11240fd0a64ded53c781
jarekk <jarokaz@hotmail.com>
2019-10-26 10:53:13 -0700

    Updates

---
+++ cicd/online_news/online_news_pipeline.py
@@ -0,0 +1,147 @@
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import tempfile
+import urllib
+
+import tensorflow as tf
+
+import tfx
+import kfp
+from tfx.components.evaluator.component import Evaluator
+from tfx.components.example_gen.csv_example_gen.component import CsvExampleGen
+from tfx.components.example_validator.component import ExampleValidator
+from tfx.components.model_validator.component import ModelValidator
+from tfx.components.pusher.component import Pusher
+from tfx.components.schema_gen.component import SchemaGen
+from tfx.components.statistics_gen.component import StatisticsGen
+from tfx.components.trainer.component import Trainer
+from tfx.components.transform.component import Transform
+from tfx.proto import evaluator_pb2
+from tfx.proto import pusher_pb2
+from tfx.proto import trainer_pb2
+
+from tfx.orchestration import metadata
+from tfx.orchestration import pipeline
+from tfx.orchestration.kubeflow import kubeflow_dag_runner
+from tfx.orchestration.kubeflow.proto import kubeflow_pb2
+
+from tfx.extensions.google_cloud_ai_platform.trainer import executor as ai_platform_trainer_executor  # pylint: disable=g-import-not-at-top
+from tfx.extensions.google_cloud_ai_platform.pusher import executor as ai_platform_pusher_executor  # pylint: disable=g-import-not-at-top
+
+from tfx.utils.dsl_utils import external_input
+
+from tfx.components.base import executor_spec
+
+from typing import Dict, List, Text
+
+def _create_pipeline(
+    pipeline_name: Text, 
+    pipeline_root: Text, 
+    data_root: Text,
+    module_file: Text,
+    beam_pipeline_args: List[Text]) -> pipeline.Pipeline:
+  """Implements the online news pipeline with TFX."""
+
+  examples = external_input(data_root)
+
+  # Brings data into the pipeline or otherwise joins/converts training data.
+  example_gen = CsvExampleGen(input_base=examples)
+
+  # Computes statistics over data for visualization and example validation.
+  statistics_gen = StatisticsGen(input_data=example_gen.outputs.examples)
+
+  # Generates schema based on statistics files.
+  infer_schema = SchemaGen(
+      stats=statistics_gen.outputs.output)
+
+  # Performs anomaly detection based on statistics and data schema.
+  validate_stats = ExampleValidator(
+      stats=statistics_gen.outputs.output, schema=infer_schema.outputs.output)
+
+# Performs transformations and feature engineering in training and serving.
+  transform = Transform(
+      input_data=example_gen.outputs.examples,
+      schema=infer_schema.outputs.output,
+      module_file=module_file)
+
+  # Uses user-provided Python function that implements a model using
+  # TensorFlow's Estimators API.
+  trainer = Trainer(
+      module_file=module_file,
+      transformed_examples=transform.outputs.transformed_examples,
+      schema=infer_schema.outputs.output,
+      transform_output=transform.outputs.transform_output,
+      train_args=trainer_pb2.TrainArgs(num_steps=10000),
+      eval_args=trainer_pb2.EvalArgs(num_steps=5000))
+
+# Uses TFMA to compute a evaluation statistics over features of a model.
+  model_analyzer = Evaluator(
+      examples=example_gen.outputs.examples,
+      model_exports=trainer.outputs.output,
+      feature_slicing_spec=evaluator_pb2.FeatureSlicingSpec(specs=[
+          evaluator_pb2.SingleSlicingSpec(
+              column_for_slicing=['weekday'])
+      ]))
+
+  # Performs quality validation of a candidate model (compared to a baseline).
+  model_validator = ModelValidator(
+      examples=example_gen.outputs.examples, model=trainer.outputs.output)
+
+  return pipeline.Pipeline(
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
+      components=[
+          example_gen, statistics_gen, infer_schema, validate_stats, transform,
+          trainer, model_analyzer, model_validator
+      ],
+      # enable_cache=True,
+      beam_pipeline_args=beam_pipeline_args
+  )
+
+
+if __name__ == '__main__':
+  # GCP project and region to be used by AI Platform services
+  _project_id = 'jk-debug1'
+  _gcp_region = 'us-central1'
+ 
+  # Connection setting to Cloud SQL based ML Metadata
+  _metadata_config = kubeflow_pb2.KubeflowMetadataConfig()
+  _metadata_config.mysql_db_service_host.environment_variable = 'MYSQL_SERVICE_HOST'
+  _metadata_config.mysql_db_service_port.environment_variable = 'MYSQL_SERVICE_PORT'
+  _metadata_config.mysql_db_name.value = 'metadb'
+  _metadata_config.mysql_db_user.value = 'root' 
+  _metadata_config.mysql_db_password.value = ''
+  
+  # GCS folder to be used to output the artifacts generated by the pipeline
+  _pipeline_name = 'online_news_pipeline_2'
+  _artifact_store_bucket = 'kfpenv-artifact-store'
+  _pipeline_root = 'gs://{}/{}/'.format(_artifact_store_bucket, _pipeline_name)
+
+  # GCS data root
+  _gcs_data_root = 'gs://{}/{}/'.format(_artifact_store_bucket, 'data')
+ 
+  # GCS path to module file
+  _gcs_module_file = 'gs://{}/{}/{}'.format(_artifact_store_bucket, 'module', 'modules.py')
+
+  runner_config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
+      kubeflow_metadata_config=_metadata_config
+  )
+
+  kubeflow_dag_runner.KubeflowDagRunner(config=runner_config).run(
+      _create_pipeline(
+          pipeline_name=_pipeline_name,
+          pipeline_root=_pipeline_root,
+          data_root=_gcs_data_root,
+          module_file=_gcs_module_file,
+          beam_pipeline_args=[]))
